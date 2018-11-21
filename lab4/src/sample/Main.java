32a6dd191cdffa366303d674d19182b3385b000e
ViachaslauPratasenia <slavaprot14@gmail.com>
2018-11-22 02:06:32 +0300

    Edit gui

--- lab4/src/sample/Main.java
+++ lab4/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Lab 4");
-        primaryStage.setScene(new Scene(root, 440, 700));
+        primaryStage.setScene(new Scene(root, 810, 350));
         primaryStage.show();
     }
 
